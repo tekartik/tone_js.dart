c808132677d82aca9abcf24a344613ffdc2cdfd1
alex <alex@tekartik.com>
2019-05-03 20:27:51 +0200

    test: travis

---
+++ tool/travis.dart
@@ -0,0 +1,18 @@
+import 'package:process_run/shell.dart';
+
+Future main() async {
+  var shell = Shell();
+
+  await shell.run('''
+
+  dartanalyzer --fatal-warnings example lib test tool
+  pub run test -p vm,firefox,chrome
+
+''');
+
+  await shell.run('''
+
+  pub run build_runner test -- -p chrome
+
+''');
+}
