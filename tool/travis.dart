6b41797f7c1e55daa28fba3a92d0a0f6726765d0
alex <alex@tekartik.com>
2021-03-23 19:03:56 +0100

    fix test

--- tool/travis.dart
+++ tool/travis.dart
@@ -1,21 +1,12 @@
+import 'package:dev_test/package.dart';
 import 'package:process_run/shell.dart';
 
 Future main() async {
+  await packageRunCi('.', options: PackageRunCiOptions(noTest: true));
   var shell = Shell();
-
   await shell.run('''
 
-  dartanalyzer --fatal-warnings example lib test tool
   pub run test -p vm,firefox,chrome
 
 ''');
-
-  /*
-  await shell.run('''
-
-  pub run build_runner test -- -p chrome
-
-''');
-
-   */
 }
